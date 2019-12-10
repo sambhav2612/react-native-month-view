165f6ef88957878f99425b41af0d384e320183b5
sambhav_2612 <sambhav@xencov.com>
2019-12-10 23:54:30 +0530

    update sccafold

--- index.js
+++ index.js
@@ -7,20 +7,14 @@ import React, {
 import {
     StyleSheet,
     View,
-    Text,
     TouchableOpacity,
-    Keyboard,
-    Platform,
+    ScrollView
 } from 'react-native';
-
-type State = {
-    keyboardUp: boolean,
-}
 
 class Tabs extends Component {
     state: State = {};
 
-    onSelect(el){
+    onSelect(el) {
         if (el.props.onSelect) {
             el.props.onSelect(el);
         } else if (this.props.onSelect) {
@@ -28,61 +22,45 @@ class Tabs extends Component {
         }
     }
 
-    componentWillMount(){
-        if (Platform.OS==='android') {
-            this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.keyboardWillShow);
-            this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.keyboardWillHide);
-        }
-    }
-
-    componentWillUnmount(){
-        this.keyboardDidShowListener.remove();
-        this.keyboardDidHideListener.remove();
-    }
-
-    keyboardWillShow = (e) => {
-        this.setState({ keyboardUp: true });
-    };
-
-    keyboardWillHide = (e) => {
-        this.setState({ keyboardUp: false });
-    };
-
-    render(){
+    render() {
         const self = this;
-        let selected = this.props.selected
-        if (!selected){
-            React.Children.forEach(this.props.children.filter(c=>c), el=>{
-                if (!selected || el.props.initial){
+        let selected = this.props.selected;
+        if (!selected) {
+            React.Children.forEach(this.props.children.filter(c => c), el => {
+                if (!selected || el.props.initial) {
                     selected = el.props.name || el.key;
                 }
             });
         }
         return (
-            <View style={[styles.tabbarView, this.props.style, this.state.keyboardUp && styles.hidden]}>
-                {React.Children.map(this.props.children.filter(c=>c),(el)=>
-                    <TouchableOpacity key={el.props.name+"touch"}
-                       testID={el.props.testID}
-                       style={[styles.iconView, this.props.iconStyle, (el.props.name || el.key) == selected ? this.props.selectedIconStyle || el.props.selectedIconStyle || {} : {} ]}
-                       onPress={()=>!self.props.locked && self.onSelect(el)}
-                       onLongPress={()=>self.onSelect(el)}
-                       activeOpacity={el.props.pressOpacity}>
-                         {selected == (el.props.name || el.key) ? React.cloneElement(el, {selected: true, style: [el.props.style, this.props.selectedStyle, el.props.selectedStyle]}) : el}
-                    </TouchableOpacity>
-                )}
+            <View style={[styles.tabbarView, this.props.style]}>
+                <ScrollView horizontal={true}>
+                    {React.Children.map(this.props.children.filter(c => c), (el) =>
+                        <TouchableOpacity key={el.props.name + "touch"}
+                            testID={el.props.testID}
+                            style={[styles.iconView, this.props.iconStyle, (el.props.name || el.key) === selected ? this.props.selectedIconStyle || el.props.selectedIconStyle || {} : {}]}
+                            onPress={() => !self.props.locked && self.onSelect(el)}
+                            onLongPress={() => self.onSelect(el)}
+                            activeOpacity={el.props.pressOpacity}>
+                            {selected === (el.props.name || el.key) ? React.cloneElement(el, {
+                                selected: true,
+                                style: [this.props.style, this.props.selectedStyle, el.props.selectedStyle]
+                            }) : el}
+                        </TouchableOpacity>
+                    )}
+                </ScrollView>
             </View>
         );
     }
 }
+
 var styles = StyleSheet.create({
     tabbarView: {
-        position:'absolute',
-        bottom:0,
-        right:0,
-        left:0,
-        height:50,
-        opacity:1,
-        backgroundColor:'transparent',
+        right: 0,
+        left: 0,
+        height: 50,
+        opacity: 1,
+        backgroundColor: 'transparent',
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center'
@@ -92,10 +70,12 @@ var styles = StyleSheet.create({
         height: 50,
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    hidden: {
-        height: 0,
-    },
+        width: 90,
+        opacity: 0.8,
+        borderBottomWidth: 2,
+        borderColor: '#10455b',
+        paddingVertical: 5
+    }
 });
 
 module.exports = Tabs;
